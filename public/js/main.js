1b70f910f8f4d2d39e2dca5eafaa1be9ad632559
IgorStetsiuk <igorstetsuk@gmail.com>
2017-10-13 09:15:58 +0300

    secpnd approach

--- public/js/main.js
+++ public/js/main.js
@@ -13,7 +13,7 @@
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
                     data = JSON.parse(xhr.responseText);
-                    callback(data);
+                    callback(data.tree);
                 } else {
                     console.error(xhr.statusText);
                 }
@@ -23,129 +23,79 @@
             console.error(xhr.statusText);
         };
         xhr.send(null);
-
     }
 
-    // function render(item) {
-    //     let postElement = document.createElement('li');
-    //     postElement.className = 'post';
-    //     let title = document.createElement('div');
-    //     title.className = 'title';
-    //     title.innerHTML = item.title;
-    //     postElement.appendChild(title);
-    //     return postElement;
+    // function rec(data, parentId) {
     //
+    //     let tree = document.createElement('ul');
+    //     tree.className=parentId;
+    //     parentId = parentId ||1;
+    //
+    //     let items = data.filter(function (el) {
+    //         return el.parent === parentId;
+    //     });
+    //     if (items.length === 0 ) return null;
+    //
+    //     console.log(items);
+    //
+    //     items.forEach(el => {
+    //         let li = document.createElement('li');
+    //         li.className = `post ${el.id}`;
+    //         let title = document.createElement('div');
+    //         title.innerHTML = el.title;
+    //
+    //         li.appendChild(title);
+    //         tree.appendChild(li);
+    //
+    //         let nestedTree = rec(data, el.id);
+    //         if (nestedTree !== null) {
+    //             li.appendChild(nestedTree);
+    //         }
+    //     });
+    //
+    //     return tree;
     // }
+
+    getRequest(getRoot);
+
+    function getRoot(data) {
+        for (let i = 0; i < data.length; i++) {
+            if (!data[i].parent) {
+                render(data[i]);
+            }
+
+            if (data[i].parent) {
+                let elem = document.getElementsByClassName(`post-${data[i].parent}`);
+                list.insertBefore(elem[0], list.children[0]);
+            }
+        }
+    }
 
     function render(item) {
-
+        let div = document.createElement('div');
+        div.className = `post-${item.id}`;
+        let title = document.createElement('div');
+        title.innerHTML = item.title;
+        div.appendChild(title);
+        list.appendChild(div);
     }
 
-    // function renderPosts(json) {
-    //
-    //     let data = json.tree;
-    //
-    //     for (let i = 0; i < data.length; i++) {
-    //
-    //         if (data[i].parent === null) {
-    //            list.appendChild(render(data[i]));
-    //
-    //
+    // function render(data) {
+    //     data.forEach((el)=>{
+    //         let newRec = rec(data,el.id);
+    //         if(newRec===null){
+    //             let ul = document.createElement('ul');
+    //             ul.className =el.id;
+    //             let title = document.createElement('div');
+    //             title.innerHTML = el.title;
+    //             ul.appendChild(title);
+    //             list.appendChild(ul);
+    //         }else {
+    //             list.appendChild(newRec);
     //         }
-    //         else  if(data[i].parent===document.getElementsByClassName(`post ${i}`)){              console.log(  document.getElementsByClassName(`post ${i}`))
-    //         }
-    //
-    //     }
-    // }
-
-    // getRequest(renderPosts);
-    //
-    //
-
-    function rec(data, parentId) {
-
-        let tree = document.createElement('ul')
-        parentId = parentId || 1;
-
-        let items = data.filter(function (el) {
-            return el.parent == parentId;
-        });
-        if (items.length == 0) return null;
-
-
-        items.forEach(el => {
-            let li = document.createElement('li');
-            li.className = `post ${el.id}`;
-
-            let title = document.createElement('div');
-            title.innerHTML = el.title;
-            li.appendChild(title);
-
-
-           tree.appendChild(li);
-
-           let nestedTree = rec(data,el.id);
-           if(nestedTree!==null ){
-               li.appendChild(nestedTree);
-           }
-        })
-
-
-
-        console.log(items);
-
-        return tree;
-
-
-    }
-
-
-    function changeStructure(json) {
-        let data = json.tree;
-
-        console.log(rec(data))
-
-    }
-
-    getRequest(changeStructure);
-
-
+    //     });
+    // };
 })();
 
-function createTree(item, parent) {
 
 
-    let item = createTree()
-}
-
-
-// root.forEach(item=>{
-//     posts.forEach(post=>{
-//         if(item.id===post.parent){
-//             item.child = post;
-//             root.push(post.id);
-//             newStructure.push(item);
-//             console.log( item.id +' '+post.id);
-//
-//         }
-//     })
-// })
-
-// let arrParrent = [];
-//
-// // data.tree.forEach((post) => {
-// //
-// //     if (!post.parent) {
-// //         let postElement = document.createElement('li');
-// //         postElement.className = 'post'+'-'+post.id;
-// //
-// //         let title = document.createElement('div');
-// //         title.className = 'title';
-// //         title.innerHTML = post.title;
-// //
-// //         postElement.appendChild(title);
-// //         list.appendChild(postElement);
-// //
-// //         arrParrent.push(post)
-// //     }
-// // });
